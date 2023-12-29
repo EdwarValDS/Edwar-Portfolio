d4424eda0d52f7936cc83908a8c57faff75be38f
Edwar Valenzuela <138516852+EdwarValDS@users.noreply.github.com>
2023-12-29 18:06:16 -0500

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import graphviz as graphviz
+
+info = {
+    'skills': ['Bussiness management', 'Data science', 'Data Analysis', 'Algorithmic trading', 
+               'Python', 'R', 'Pinescript', 'Streamlit']
+}
